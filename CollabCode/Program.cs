d8122abf809a961477e726edce1f5f97f7c648fb
Rutuja Pansare <Rutuja.Pansare@lntinfotech.com>
2020-09-09 17:37:44 +0530

    Code for add and multiply numbers

---
+++ CollabCode/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter num1:");
+                    double num1 = Convert.ToInt64(Console.ReadLine());
+                    Console.WriteLine("Enter num2:");
+                    double num2 = Convert.ToInt64(Console.ReadLine());
+                    if (num1 != 0 && num2 != 0)
+                    {
+                        new Program().sum(num1, num2);
+                        new Program().product(num1, num2);
+                        Console.ReadKey();
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Number 0 not allowed");
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+            }
+            
+
+        }
+
+        public void sum(double num1, double num2)
+        {
+            Console.WriteLine("Sum is "+num1+num2);
+        }
+        public void product(double num1, double num2)
+        {
+            Console.WriteLine("Product is "+num1 * num2);
+        }
+    }
+}
